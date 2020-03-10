f70ca3f5f945edc9daa4a6b9bb73cf656406082f
YoonSeongKyeong <mdy6099@naver.com>
2020-03-10 10:29:43 +0900

    add interfaces for controller api

--- server/src/interface/api.ts
+++ server/src/interface/api.ts
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/camelcase */
 // 서버 API와 관련된 interface를 모아둡니다.
-import { Item_Dependency, Option_Dependency, Shipping_Dependency } from './dependency';
+import {
+  Item_Dependency,
+  Option_Dependency,
+  Shipping_Dependency,
+  Wish_Dependency,
+  Purchased_Dependency,
+} from './dependency';
 
 export interface GetItemsReq extends Item_Dependency {
   q: string;
@@ -8,40 +14,56 @@ export interface GetItemsReq extends Item_Dependency {
   limit: string;
 }
 
-interface Options extends Option_Dependency {
+export interface Options extends Option_Dependency {
   id: number;
   color: string;
   size: string;
   stock: number;
 }
 
-interface Shipping extends Shipping_Dependency {
+export interface Shipping extends Shipping_Dependency {
   method: string;
   price: number;
   canBundle: boolean;
 }
 
-export interface ItemToGet extends Item_Dependency {
-  id: 1;
-  name: 'Python Hood T-Shirts';
-  provider: 'StyleShare';
-  price: 20000;
+export interface ItemForm extends Item_Dependency {
+  id: number;
+  name: string;
+  provider: string;
+  price: number;
   options: Options[];
   shipping: Shipping;
 }
 
 export interface GetItemsRes {
-  goods: ItemToGet[];
+  goods: ItemForm[];
 }
 
-export interface GetMyCartReq {}
+export interface GetMyCartRes {
+  goods: ItemForm[];
+}
 
-export interface GetMyCartRes {}
+export interface PurchaseItemReq extends Purchased_Dependency {
+  goods: ItemForm[];
+}
 
-export interface PurchaseItemReq {}
+export interface PurchaseItemRes extends Item_Dependency, Purchased_Dependency {
+  isSuccess: boolean;
+  price: number;
+}
 
-export interface PurchaseItemRes {}
+export interface PostMyCartReq extends Item_Dependency, Wish_Dependency {
+  itemIdList: string[];
+}
 
-export interface PutMyCartReq {}
+export interface PostMyCartRes extends Wish_Dependency {
+  isSuccess: boolean;
+}
+export interface DeleteMyCartReq extends Item_Dependency, Wish_Dependency {
+  itemIdList: string[];
+}
 
-export interface PutMyCartRes {}
+export interface DeleteMyCartRes extends Wish_Dependency {
+  isSuccess: boolean;
+}
