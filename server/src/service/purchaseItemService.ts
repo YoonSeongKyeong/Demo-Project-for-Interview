8fd92e975f464f7d6f307df137950d3a800935c4
YoonSeongKyeong <mdy6099@naver.com>
2020-03-12 01:28:17 +0900

    use deadlock free sorted id update list

--- server/src/service/purchaseItemService.ts
+++ server/src/service/purchaseItemService.ts
@@ -29,6 +29,8 @@ export class PurchaseItemService {
   }: PurchaseItemService_PurchaseByItemFormListAndUserIdInput): Promise<
     PurchaseItemService_PurchaseByItemFormListAndUserIdOutput
   > => {
+    itemFormList.sort((a, b) => b.id - a.id); // update 시 Deadlock을 방지하기 위해서 itemId 오름차순으로 sort한다.
+
     // 실제로는 외부 API를 사용할 것이므로 현재 구현한 것에서 변경이 필요할 것이다.
     // 상품 구매 form의 상품 정보가 구매 직전 상품 정보와 일치하지 않거나 재고보다 많은 양을 구매하려고 할 때 구매가 실패한다.
     // 결제모듈에 맞게 정합성을 엄격하게 체크하되, 고객이 상품 구매 마지막 단계에서 불편을 느끼지 않도록 유연한 디자인이 필요하다.
@@ -38,7 +40,7 @@ export class PurchaseItemService {
     const itemIdList = itemFormList.map((itemForm: ItemForm) => itemForm.id);
 
     // 중간에 item의 가격 및 shipping 옵션이 바뀌는 경우에 totalPrice가 invalid할 수 있다. 이 경우엔 상품 구매를 취소할지, 아니면 새로 바뀐 상품 가격으로 진행할지, 예전 상품 가격으로 결제를 진행하며 상품 가격 변경 시 판매자에게 양해를 부탁할지 결정해야 한다.
-    await getConnection().transaction(async transactionalEntityManager => {
+    await getConnection().transaction('READ COMMITTED', async transactionalEntityManager => {
       const user = await transactionalEntityManager.findOne(User, { id: userId });
       if (!user) {
         throw new Error('Invalid User Id');
@@ -50,7 +52,7 @@ export class PurchaseItemService {
         relations: ['provider', 'shipping', 'options'],
       });
       for (let i = 0; i < itemFormList.length; i++) {
-        // 각 itemForm에 대해 실제 item과 비교해서 유효성 테스트를 한 후 요청한 상품 개수만큼 재고에서 빼준다.
+        // 각 itemForm에 대해 실제 item과 비교해서 유효성 테스트를 한 후 요청한 상품 개수만큼 재고에서 빼준다. ( Entity에 stock >= 0 제약조건을 걸어두어서 consistency가 보장된다. )
         const itemForm = itemFormList[i];
         const item = items[i];
         if (!validateItemFormToItem({ itemForm, item })) {
