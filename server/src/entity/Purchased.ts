82c397fe23a818bcc879884d1ae85d86e1cedbe5
YoonSeongKyeong <mdy6099@naver.com>
2020-03-11 20:27:12 +0900

    add description for status in Entity-Purchased

--- server/src/entity/Purchased.ts
+++ server/src/entity/Purchased.ts
@@ -8,6 +8,13 @@ export class Purchased {
   purchasedAt: Date; // 구매한 시각
 
   @Column()
+  // WAITING_FOR_SHIPPING : 배송 대기중
+  // GOING_HOME : 배송 진행중
+  // RECIEVED : 배송 완료
+  // REQUESTED_FOR_CHANGE : 교환 대기중
+  // CHANGED : 교환 완료
+  // REQUESTED_FOR_REFUND : 환불 대기중
+  // REFUNDED : 환불 완료
   status: string; // 상품 상태
 
   @Column()
