b466c0b72aa6a0f69e70d9eaf7cde1842d0bed48
YoonSeongKyeong <mdy6099@naver.com>
2020-03-12 12:27:59 +0900

    refactor logic of deleteMyCart

--- server/src/controller/deleteMyCart.ts
+++ server/src/controller/deleteMyCart.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { DeleteMyCartReq, DeleteMyCartRes } from '../interface/api';
-import { TokenForWish, TokenForAuth } from '../interface/serversideSpecific';
+import { TokenForWish, TokenForAuth, ItemIdList } from '../interface/serversideSpecific';
 import { UserService } from '../service/UserService';
 import { WishService } from '../service/WishService';
 import { configs } from '../utils/configs';
@@ -11,18 +11,31 @@ import { signJWT } from '../utils/signJWT';
 
 export async function deleteMyCart(request: Request, response: Response): Promise<void> {
   debugger;
-  let reqBody: DeleteMyCartReq;
+  const reqBody: DeleteMyCartReq = { itemIdList: request.body.itemIdList }; // interface 외의 정보 제거
   let resBody: DeleteMyCartRes;
 
-  const { itemIdList } = request.body; // interface 외의 정보 제거
-  // eslint-disable-next-line prefer-const
-  reqBody = { itemIdList };
+  let itemIdList: ItemIdList = [];
 
   try {
+    // 쿠키의 장바구니 정보 확인
     const wish = (await extractJWT('wish', request)) as TokenForWish;
-    const { itemIdList } = wish;
-
+    itemIdList = wish.itemIdList;
+  } catch (error) {
+    if (
+      error.message === 'Invalid Token Name [wish]' ||
+      error.message === 'cannot find token in cookie [wish]'
+    ) {
+      // 장바구니 정보가 없는 경우 빈 배열을 사용한다.
+    } else {
+      console.log('ERROR: ' + error.message);
+      response.status(500).send(error.message);
+      return;
+    }
+  } finally {
+    // itemIdList에서 삭제 요청된 itemIdList를 지운다.
+    itemIdList = itemIdList.filter(id => !reqBody.itemIdList.includes(id));
     try {
+      // 로그인 정보를 받아온다.
       const auth = (await extractJWT('auth', request)) as TokenForAuth;
       const { id } = auth;
 
@@ -31,22 +44,7 @@ export async function deleteMyCart(request: Request, response: Response): Promis
 
       if (id && (await userService.isValidUser(id))) {
         // 로그인이 되어 있다면, Wish 목록에서 삭제 요청된 상품들을 지운다.
-        await wishService.deleteItemIdListOfUser({ itemIdList, userId: id });
-        // 기본적으로 장바구니 토큰에서 삭제 요청된 상품들을 지운다.
-        reqBody.itemIdList.forEach(id => {
-          // 현재는 장바구니에 추가하는 상품 수가 별로 없어서 O(N^2) 알고리즘을 쓰지만, 만약 scalability가 필요하다면, 오름차순을 유지하며 binarysearch로 O(NlgN)만에 처리할 수 있다. or 두 배열을 병합한 뒤 sort하고 중복된 것을 제외하고 넣어도 O(NlgN)
-          if (itemIdList.includes(id)) {
-            itemIdList.splice(itemIdList.indexOf(id), 1);
-          }
-        });
-        response.cookie('wish', signJWT({ itemIdList }), {
-          expires: new Date(Date.now() + 900000000000),
-          httpOnly: true,
-          domain: configs.CLIENT_DOMAIN,
-          path: '/',
-        });
-        resBody = { isSuccess: true };
-        response.status(200).json(resBody);
+        await wishService.deleteItemIdListOfUser({ itemIdList: reqBody.itemIdList, userId: id });
       } else {
         throw new Error('Invalid User Id');
       }
@@ -61,33 +59,14 @@ export async function deleteMyCart(request: Request, response: Response): Promis
           // id가 유효하지 않은 경우, Clear Cookie
           response.clearCookie('auth', { domain: configs.CLIENT_DOMAIN, path: '/' });
         }
-        // 기본적으로 장바구니 토큰에서 삭제 요청된 상품들을 지운다.
-        reqBody.itemIdList.forEach(id => {
-          // 현재는 장바구니에 추가하는 상품 수가 별로 없어서 O(N^2) 알고리즘을 쓰지만, 만약 scalability가 필요하다면, 오름차순을 유지하며 binarysearch로 O(NlgN)만에 처리할 수 있다. or 두 배열을 병합한 뒤 sort하고 중복된 것을 제외하고 넣어도 O(NlgN)
-          if (itemIdList.includes(id)) {
-            itemIdList.splice(itemIdList.indexOf(id), 1);
-          }
-        });
-        response.cookie('wish', signJWT({ itemIdList }), {
-          expires: new Date(Date.now() + 900000000000),
-          httpOnly: true,
-          domain: configs.CLIENT_DOMAIN,
-          path: '/',
-        });
-        resBody = { isSuccess: true };
-        response.status(200).json(resBody);
       } else {
         console.log('ERROR: ' + error.message);
         response.status(500).send(error.message);
+        return;
       }
-    }
-  } catch (error) {
-    if (
-      error.message === 'Invalid Token Name [wish]' ||
-      error.message === 'cannot find token in cookie [wish]'
-    ) {
-      // 장바구니 정보가 없는 경우 새로 만들어준다.
-      response.cookie('wish', signJWT([]), {
+    } finally {
+      // 쿠키의 장바구니를 업데이트한다.
+      response.cookie('wish', signJWT({ itemIdList }), {
         expires: new Date(Date.now() + 900000000000),
         httpOnly: true,
         domain: configs.CLIENT_DOMAIN,
@@ -95,9 +74,6 @@ export async function deleteMyCart(request: Request, response: Response): Promis
       });
       resBody = { isSuccess: true };
       response.status(200).json(resBody);
-    } else {
-      console.log('ERROR: ' + error.message);
-      response.status(500).send(error.message);
     }
   }
 }
