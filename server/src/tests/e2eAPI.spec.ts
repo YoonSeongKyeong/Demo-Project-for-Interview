9ca520fc88100c1db8b67621a6ad6fa1dce2e2b1
YoonSeongKyeong <mdy6099@naver.com>
2020-03-14 18:55:10 +0900

    add tests for GetItems

--- server/src/tests/e2eAPI.spec.ts
+++ server/src/tests/e2eAPI.spec.ts
@@ -9,7 +9,7 @@ import { setConfigure, configs } from '../utils/configs';
 import { CreateServer } from '../interface/serversideSpecific';
 import { prepareTestData } from '../utils/prepareTestData';
 import { isConformToInterface } from '../utils/isConformToInterface';
-import { GetItemsReq, GetItemsRes } from '../interface/api';
+import { GetItemsReq, GetItemsRes, ItemForm } from '../interface/api';
 
 // declare server core variables
 let app: Application;
@@ -87,6 +87,125 @@ describe('Integration API Test: ', () => {
         .expect('Content-Type', 'application/json; charset=utf-8')
         .then(res => {
           expect(isConformToInterface(res.body, resBody)).toBeTruthy();
+        });
+    });
+    it('GET items with name containing the query keyword which is specified in request', async () => {
+      const searchKeyword = 'SEARCH Tester';
+      const reqBody: GetItemsReq = {
+        q: searchKeyword,
+      };
+      const resBody: GetItemsRes = {
+        goods: [
+          {
+            id: 0,
+            name: '',
+            titleImage: '',
+            price: 0,
+            provider: '',
+            options: [],
+            shipping: { method: '', price: 0, canBundle: true },
+          },
+        ],
+      };
+      return await request(app)
+        .get('/api/items')
+        .set('Content-Type', 'application/json')
+        .query(reqBody)
+        .send()
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .then(res => {
+          expect(isConformToInterface(res.body, resBody)).toBeTruthy();
+          res.body.goods.forEach((item: ItemForm) =>
+            expect(item.name.indexOf(searchKeyword) !== -1).toBeTruthy(),
+          ); // all item name includes the query keyword
+        });
+    });
+    it('GET items with not specified default offset, which is equal to items with offset = 0 ', async () => {
+      const offsetZero = '0';
+      const reqBody: GetItemsReq = {};
+      const newReqBody: GetItemsReq = { offset: offsetZero };
+      const resBody: GetItemsRes = {
+        goods: [
+          {
+            id: 0,
+            name: '',
+            titleImage: '',
+            price: 0,
+            provider: '',
+            options: [],
+            shipping: { method: '', price: 0, canBundle: true },
+          },
+        ],
+      };
+      return await request(app)
+        .get('/api/items')
+        .set('Content-Type', 'application/json')
+        .query(reqBody)
+        .send()
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .then(async res => {
+          expect(isConformToInterface(res.body, resBody)).toBeTruthy();
+          const resBodyWithDefaultOffset = res.body;
+          await request(app)
+            .get('/api/items')
+            .set('Content-Type', 'application/json')
+            .query(newReqBody)
+            .send()
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .then(newRes => {
+              expect(isConformToInterface(newRes.body, resBody)).toBeTruthy();
+              const resBodyWithZeroOffset = newRes.body;
+              expect(resBodyWithDefaultOffset).toMatchObject(resBodyWithZeroOffset);
+            });
+        });
+    });
+    it('GET items with specific offset = 1, which is not equal to items with offset = 2', async () => {
+      const offsetOne = '1';
+      const offsetTwo = '2';
+      const reqBody: GetItemsReq = { offset: offsetOne };
+      const newReqBody: GetItemsReq = { offset: offsetTwo };
+      const resBody: GetItemsRes = {
+        goods: [
+          {
+            id: 0,
+            name: '',
+            titleImage: '',
+            price: 0,
+            provider: '',
+            options: [],
+            shipping: { method: '', price: 0, canBundle: true },
+          },
+        ],
+      };
+      return await request(app)
+        .get('/api/items')
+        .set('Content-Type', 'application/json')
+        .query(reqBody)
+        .send()
+        .expect(200)
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .then(async res => {
+          expect(isConformToInterface(res.body, resBody)).toBeTruthy();
+          const itemListWithOffsetOne = res.body.goods;
+          await request(app)
+            .get('/api/items')
+            .set('Content-Type', 'application/json')
+            .query(newReqBody)
+            .send()
+            .expect(200)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .then(newRes => {
+              expect(isConformToInterface(newRes.body, resBody)).toBeTruthy();
+              const itemListWithOffsetTwo = newRes.body.goods;
+              expect(itemListWithOffsetOne.length).toEqual(itemListWithOffsetTwo.length);
+              const len = itemListWithOffsetOne.length;
+              for (let i = 0; i < len; i++) {
+                expect(itemListWithOffsetOne[i].id).not.toEqual(itemListWithOffsetTwo[i].id);
+              }
+            });
         });
     });
   });
