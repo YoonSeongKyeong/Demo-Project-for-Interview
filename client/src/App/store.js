3d3b6eb61c00f080dc066399cca4ad18855c5a7b
YoonSeongKyeong <mdy6099@naver.com>
2020-03-18 20:51:35 +0900

    use explicit store file in src

--- client/src/App/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducer';
-import thunk from 'redux-thunk';
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-export default createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
