8a3f18a035e6aeed002dbe11cbb290556b9429c2
YoonSeongKyeong <mdy6099@naver.com>
2020-03-19 20:08:06 +0900

    add shoppingcart wishlist a functions & props

--- client/src/components/ShoppingCart_Item/index.tsx
+++ client/src/components/ShoppingCart_Item/index.tsx
@@ -1,21 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Checkbox, Tag, Select, InputNumber, Button } from 'antd';
+
 import { ItemForm } from '../../interface/api';
 import './style.css';
+import { addOptionToPurchaseListActionInput } from '../../modules/shoppingCart';
 
 const { Option } = Select;
 // ShoppingCart_Item: 상품 사진, 상품 이름, 판매자 이름, 가격 및 재고를 보여준다. 배송방식을 보여준다. 상품을 선택할 수 있다. 상품을 제거할 수 있다.
 // 다양한 옵션을 dropdown으로 선택할 수 있다. 구매할 상품 갯수를 선택해서 제출할 수 있다..
 
-const ShoppingCart_Item: React.FC<{ item: ItemForm }> = ({ item }) => {
+type itemProps = {
+  item: ItemForm;
+  onDeleteItemFromWishList: (itemId: number) => void;
+  onAddOptionToPurchaseList: ({
+    itemId,
+    option,
+  }: addOptionToPurchaseListActionInput) => void;
+  onSelect: (itemId: number) => void;
+  onUnSelect: (itemId: number) => void;
+  isChecked: boolean;
+};
+
+const ShoppingCart_Item: React.FC<itemProps> = ({
+  item,
+  onDeleteItemFromWishList,
+  onAddOptionToPurchaseList,
+  onSelect,
+  onUnSelect,
+  isChecked,
+}) => {
+  const [selectedOptionIndex, setSeletedOptionIndex] = useState(0);
+  const [numOfItemsToBuy, setNumOfItemsToBuy] = useState(0);
+
+  const onChangeCheckBox = () => {
+    if (isChecked) {
+      onUnSelect(item.id);
+    } else {
+      onSelect(item.id);
+    }
+  };
+
+  const minNum = 0;
+  const maxNum = item.options[selectedOptionIndex].stock;
+
+  const onChangeNumInput = (value: number) => {
+    if (value >= minNum && value <= maxNum) {
+      setNumOfItemsToBuy(value);
+    }
+  };
+
+  const onDelete = () => {
+    onDeleteItemFromWishList(item.id);
+  };
+
+  const onAdd = () => {
+    onAddOptionToPurchaseList({
+      itemId: item.id,
+      option: {
+        ...item.options[selectedOptionIndex],
+        stock: numOfItemsToBuy,
+      },
+    });
+  };
+
   return (
     <Card
       style={{ marginTop: '1rem' }}
       type="inner"
       title={item.name}
-      extra={<Checkbox />}
+      extra={<Checkbox checked={isChecked} onChange={onChangeCheckBox} />}
     >
-      <img className="small-img" src={item.titleImage} />
+      <img className="small-img" src={item.titleImage} alt="" />
       <div>
         <div className="item-info ">
           <span className="margin-1rem">
@@ -59,9 +114,13 @@ const ShoppingCart_Item: React.FC<{ item: ItemForm }> = ({ item }) => {
           )}
         </div>
         <div className="">
-          <Select defaultValue="Choose The Option" className="select-option">
-            {item.options.map(option => (
-              <Option value={option.id}>
+          <Select
+            defaultValue={0}
+            className="select-option"
+            onChange={value => setSeletedOptionIndex(value)}
+          >
+            {item.options.map((option, index) => (
+              <Option value={index}>
                 <Tag color="volcano">size</Tag>
                 <Tag color="volcano">{option.size}</Tag>
                 <Tag color="red">color</Tag>
@@ -71,19 +130,28 @@ const ShoppingCart_Item: React.FC<{ item: ItemForm }> = ({ item }) => {
           </Select>
           <InputNumber
             className="margin-1rem"
-            min={1}
-            max={10}
-            defaultValue={1}
+            min={minNum}
+            max={maxNum}
+            defaultValue={minNum}
+            onChange={onChangeNumInput}
           />
           <span className="margin-1rem">
             <Tag color="gold">Stock</Tag>
-            <Tag color="gold">10</Tag>
+            <Tag color="gold">{maxNum}</Tag>
           </span>
         </div>
-        <Button type="danger" className="float-right margin-1rem">
+        <Button
+          type="danger"
+          className="float-right margin-1rem"
+          onClick={onDelete}
+        >
           Delete From Wish List
         </Button>
-        <Button type="primary" className="float-right margin-1rem">
+        <Button
+          type="primary"
+          className="float-right margin-1rem"
+          onClick={onAdd}
+        >
           Add To Purchase List
         </Button>
       </div>
